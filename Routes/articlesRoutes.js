43e816c12373011c3ef5d9407fa6f20eddd84242
EMMADEV <kamdememmanuel10@gmail.com>
2026-03-23 16:26:56 +0100

    Update articlesRoutes.js

--- Routes/articlesRoutes.js
+++ Routes/articlesRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('../controllers/articleController');
+const controller = require('../Controllers/articleController');
 
 /**
  * @swagger
