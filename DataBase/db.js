c6dfdf665100bc4828d54e565eac43649f04537a
kamdememmanuel10-ops <kamdememmanuel10@gmail.com>
2026-03-29 19:28:53 +0200

    fix db.js better-sqlite3 syntax

--- DataBase/db.js
+++ DataBase/db.js
@@ -2,16 +2,17 @@ const Database = require('better-sqlite3');
 
 const db = new Database('./blog.db', {verbose: console.log });
 
-db.serialize(() => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            titre TEXT,
-            contenu TEXT,
-            auteur TEXT,
-            date TEXT,
-            categorie TEXT,
-            tags TEXT
-            )
-        `);
-});
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS articles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        titre TEXT,
+        contenu TEXT,
+        auteur TEXT,
+        date TEXT,
+        categorie TEXT,
+        tags TEXT
+    )
+`);
+
+module.exports = db;
